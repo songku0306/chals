edca482ca5d5b9adb91813320d1da84c0609f8dd
songku0306 <songku0306@gmail.com>
2021-03-10 02:55:06 +0900

    sd update

--- Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
+++ Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.goldtestt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import java.util.*
-import kotlin.concurrent.timer
 
 
 class MainActivity : AppCompatActivity() {
@@ -35,8 +30,8 @@ class MainActivity : AppCompatActivity() {
         var tv_gold : Int = 0
 
         gl1 = findViewById(R.id.gl1)
-        gl10 = findViewById(R.id.gl10)
-        gl100 = findViewById(R.id.gl100)
+        gl10 = findViewById(R.id.brown_1)
+        gl100 = findViewById(R.id.plat_1)
         sd1000 = findViewById(R.id.sd1000)
         sd5000 = findViewById(R.id.sd5000)
         sd101 = findViewById(R.id.sd101)
--- Wormractice/app/src/main/java/com/example/wormractice/WormDatabase.kt
+++ Wormractice/app/src/main/java/com/example/wormractice/WormDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
-@Database(entities = arrayOf(WormEntity::class), version = 1)
+@Database(entities = arrayOf(WormEntity::class), version = 2)
 abstract class WormDatabase : RoomDatabase() {
 
     abstract fun wormDAO() : WormDAO
