d59074fb3eb7bdae2b24bfe5fda02f02444220ba
songku0306 <songku0306@gmail.com>
2021-03-15 22:13:25 +0900

    꾸꾸는 왜그럴까

--- CalcurateTest/app/src/main/java/com/example/calcuratetest/MainActivity.kt
+++ CalcurateTest/app/src/main/java/com/example/calcuratetest/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.calcuratetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btn1 : Button
     lateinit var tv1 : TextView
 
+    var num = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +21,12 @@ class MainActivity : AppCompatActivity() {
         btn1 = findViewById(R.id.btn1)
         tv1 = findViewById(R.id.tv1)
 
-        var tv1 = 0
 
 
         btn1.setOnClickListener {
-            tv1 += 1
+           num += 4400
+           tv1.text = num.toString()
         }
+
     }
 }
--- Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
+++ Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
@@ -12,36 +12,81 @@ class MainActivity : AppCompatActivity() {
     lateinit var tv_gold : TextView
     lateinit var tv_seed : TextView
 
-    lateinit var silver_1 : Button
+    lateinit var btn_reset : Button
     lateinit var gold_1 : Button
     lateinit var brown_1 : Button
     lateinit var plat_1 : Button
-    lateinit var sd_11 : Button
     lateinit var sd_44 : Button
     lateinit var sd_22 : Button
     lateinit var sd_444 : Button
+
+    var gold = 0
+    var seed = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv_gold = findViewById(R.id.tv_gold)
-        tv_seed = findViewById(R.id.tv_seed)
+        tv_gold = findViewById(R.id.rhl)
+        tv_seed = findViewById(R.id.tlem)
 
-        var tv_seed : Int = 0
-        var tv_gold : Int = 0
-
-        silver_1 = findViewById(R.id.silver_1)
+        btn_reset = findViewById(R.id.btn_reset)
         gold_1 = findViewById(R.id.gold_1)
         brown_1 = findViewById(R.id.brown_1)
         plat_1 = findViewById(R.id.plat_1)
-        sd_11 = findViewById(R.id.sd_11)
         sd_44 = findViewById(R.id.sd_44)
         sd_22 = findViewById(R.id.sd_22)
         sd_444 = findViewById(R.id.sd_444)
 
 
+        btn_reset.setOnClickListener {
+            gold = 0
+            tv_gold.text = gold.toString()
+            seed = 0
+            tv_seed.text = seed.toString()
+        }
 
+        gold_1.setOnClickListener {
+            gold += 1
+            tv_gold.text = gold.toString()
+            seed += 4400
+            tv_seed.text = seed.toString()
+        }
+
+        brown_1.setOnClickListener {
+            gold += 5
+            tv_gold.text = gold.toString()
+            seed += 22000
+            tv_seed.text = seed.toString()
+        }
+
+        plat_1.setOnClickListener {
+            gold += 10
+            tv_gold.text = gold.toString()
+            seed += 44000
+            tv_seed.text = seed.toString()
+        }
+
+        sd_44.setOnClickListener {
+            gold += 1
+            tv_gold.text = gold.toString()
+            seed += 4400
+            tv_seed.text = seed.toString()
+        }
+
+        sd_22.setOnClickListener {
+            gold += 5
+            tv_gold.text = gold.toString()
+            seed += 22000
+            tv_seed.text = seed.toString()
+        }
+
+        sd_444.setOnClickListener {
+            gold += 10
+            tv_gold.text = gold.toString()
+            seed += 44000
+            tv_seed.text = seed.toString()
+        }
     }
 
 
