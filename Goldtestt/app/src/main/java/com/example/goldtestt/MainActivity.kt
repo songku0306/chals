adeef0aa9be21a89986a5ad179afa15f696ed587
songku0306 <songku0306@gmail.com>
2021-03-17 21:37:38 +0900

    set all

--- Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
+++ Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
@@ -21,9 +21,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var plus1 : ImageButton
     lateinit var plus2 : ImageButton
     lateinit var plus3 : ImageButton
+    lateinit var plus4 : ImageButton
     lateinit var minus1 : ImageButton
     lateinit var minus2 : ImageButton
     lateinit var minus3 : ImageButton
+    lateinit var minus4 : ImageButton
 
     lateinit var gold_1 : ImageButton
     lateinit var brown_1 : ImageButton
@@ -47,12 +49,15 @@ class MainActivity : AppCompatActivity() {
         plus1 = findViewById(R.id.plus1)
         plus2 = findViewById(R.id.plus2)
         plus3 = findViewById(R.id.plus3)
+        
+        plus4 = findViewById(R.id.plus4)
         gold_1 = findViewById(R.id.gold_1)
         brown_1 = findViewById(R.id.brown_1)
         plat_1 = findViewById(R.id.plat_1)
         minus1 = findViewById(R.id.minus1)
         minus2 = findViewById(R.id.minus2)
         minus3 = findViewById(R.id.minus3)
+        minus4 = findViewById(R.id.minus4)
 
         btn_reset.setOnClickListener {
             gold = 0
@@ -79,6 +84,12 @@ class MainActivity : AppCompatActivity() {
             gold += 10
             tv_gold.text = gold.toString()
             seed += 44000
+            tv_seed.text = seed.toString()
+        }
+        plus4.setOnClickListener {
+            gold += 50
+            tv_gold.text = gold.toString()
+            seed += 220000
             tv_seed.text = seed.toString()
         }
 
@@ -123,7 +134,12 @@ class MainActivity : AppCompatActivity() {
             seed -= 44000
             tv_seed.text = seed.toString()
         }
-
+        minus4.setOnClickListener {
+            gold -= 50
+            tv_gold.text = gold.toString()
+            seed -= 220000
+            tv_seed.text = seed.toString()
+        }
 
 
         mInterstitialAd = InterstitialAd(this)
