ec16814316adfc527576a1284bd65672f1c314bd
songku0306 <songku0306@gmail.com>
2021-03-17 18:36:39 +0900

    set all

--- Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
+++ Goldtestt/app/src/main/java/com/example/goldtestt/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.goldtestt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import com.google.android.gms.ads.*
 import java.util.*
 
 
@@ -14,36 +18,67 @@ class MainActivity : AppCompatActivity() {
     lateinit var tv_seed : TextView
 
     lateinit var btn_reset : ImageButton
+    lateinit var plus1 : ImageButton
+    lateinit var plus2 : ImageButton
+    lateinit var plus3 : ImageButton
+    lateinit var minus1 : ImageButton
+    lateinit var minus2 : ImageButton
+    lateinit var minus3 : ImageButton
+
     lateinit var gold_1 : ImageButton
     lateinit var brown_1 : ImageButton
     lateinit var plat_1 : ImageButton
-    lateinit var sd_44 : Button
-    lateinit var sd_22 : Button
-    lateinit var sd_444 : Button
 
     var gold = 0
     var seed = 0
 
+    lateinit var adbtn : ImageButton
+    private lateinit var mInterstitialAd: InterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         tv_gold = findViewById(R.id.rhl)
         tv_seed = findViewById(R.id.tlem)
 
         btn_reset = findViewById(R.id.btn_reset)
+        plus1 = findViewById(R.id.plus1)
+        plus2 = findViewById(R.id.plus2)
+        plus3 = findViewById(R.id.plus3)
         gold_1 = findViewById(R.id.gold_1)
         brown_1 = findViewById(R.id.brown_1)
         plat_1 = findViewById(R.id.plat_1)
-        sd_44 = findViewById(R.id.sd_44)
-        sd_22 = findViewById(R.id.sd_22)
-        sd_444 = findViewById(R.id.sd_444)
-
+        minus1 = findViewById(R.id.minus1)
+        minus2 = findViewById(R.id.minus2)
+        minus3 = findViewById(R.id.minus3)
 
         btn_reset.setOnClickListener {
             gold = 0
             tv_gold.text = gold.toString()
             seed = 0
+            tv_seed.text = seed.toString()
+        }
+
+        plus1.setOnClickListener {
+            gold += 1
+            tv_gold.text = gold.toString()
+            seed += 4400
+            tv_seed.text = seed.toString()
+        }
+
+        plus2.setOnClickListener {
+            gold += 5
+            tv_gold.text = gold.toString()
+            seed += 22000
+            tv_seed.text = seed.toString()
+        }
+
+        plus3.setOnClickListener {
+            gold += 10
+            tv_gold.text = gold.toString()
+            seed += 44000
             tv_seed.text = seed.toString()
         }
 
@@ -68,31 +103,42 @@ class MainActivity : AppCompatActivity() {
             tv_seed.text = seed.toString()
         }
 
-        sd_44.setOnClickListener {
-            gold += 1
+        minus1.setOnClickListener {
+            gold -= 1
             tv_gold.text = gold.toString()
-            seed += 4400
+            seed -= 4400
             tv_seed.text = seed.toString()
         }
 
-        sd_22.setOnClickListener {
-            gold += 5
+        minus2.setOnClickListener {
+            gold -= 5
             tv_gold.text = gold.toString()
-            seed += 22000
+            seed -= 22000
             tv_seed.text = seed.toString()
         }
 
-        sd_444.setOnClickListener {
-            gold += 10
+        minus3.setOnClickListener {
+            gold -= 10
             tv_gold.text = gold.toString()
-            seed += 44000
+            seed -= 44000
             tv_seed.text = seed.toString()
         }
+
+
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/8691691433"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+        adbtn = findViewById(R.id.adbtn)
+        adbtn.setOnClickListener(View.OnClickListener {
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+        })
     }
-
-
-
-
 
 
 
