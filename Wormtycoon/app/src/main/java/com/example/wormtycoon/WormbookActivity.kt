7ea1915ce518237b4f464da92a3de8aa5d3e73e6
songku0306 <songku0306@gmail.com>
2021-04-22 19:36:26 +0900

    made it

---
+++ Wormtycoon/app/src/main/java/com/example/wormtycoon/WormbookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wormtycoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WormbookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wormbook)
+    }
+}
